9274109cbb3e9ac8a961aee9e5b5632f8a50fa12
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-04-08 20:32:08 +0200

    feat: configure it to be used as a cli

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 #! /usr/bin/env node
 
-require("dotenv").config();
-const { Configuration, OpenAIApi } = require("openai");
+import { config } from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
+
+import requestSnippet from "./snippet-requester.js";
+
+config();
 
 if (!process.env.OPENAI_API_KEY) {
   console.error("Missing OPENAI_API_KEY environment variable");
@@ -14,53 +18,12 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const PROMPT_TEMPLATE = `Take my request and generate a code snippet.
-Request: "bash: Check if a variable is not empty"
-Snippet:if [ -n "$my_variable" ]; then
-    echo "my_variable is not empty"
-else
-    echo "my_variable is empty"
-fi
-
-Request: "array reduce with short explaining comments in js"
-Snippet:// reduce takes an array and reduces it to a single value
-const array = [1, 2, 3, 4];
-const reducedValue = array.reduce((accumulator, currentValue) => {
-  // accumulator is the running total of the reduction
-  // currentValue is the current element being processed in the array
-  return accumulator + currentValue;
-}, 0); // 0 is the initial value of the accumulator
-
-console.log(reducedValue); // 10
-
-Request: "Print all variables of a list, in pseudo-code"
-Snippet:// list is an array of variables
-for each variable in list:
-    print variable
-end
----
-Request: "{request}"
-Snippet:`;
-
-const requestSnippet = async (request) => {
-  const { data: response } = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: PROMPT_TEMPLATE.replace("{request}", request),
-    temperature: 0,
-    max_tokens: 1600,
-    top_p: 1,
-    frequency_penalty: 0.2,
-    presence_penalty: 0,
-    stop: ["---"],
-  });
-
-  console.log(response.choices[0]?.text);
-};
-
 process.stdin.on("data", async (data) => {
-  if (data.length < 2) {
+  const snippet = await requestSnippet(data?.toString(), openai);
+
+  if (!snippet) {
     return;
   }
 
-  await requestSnippet(data.toString());
+  console.log(snippet);
 });
---
+++ src/snippet-requester.js
@@ -0,0 +1,49 @@
+const PROMPT_TEMPLATE = `Take my request and generate a code snippet.
+Request: "bash: Check if a variable is not empty"
+Snippet:if [ -n "$my_variable" ]; then
+    echo "my_variable is not empty"
+else
+    echo "my_variable is empty"
+fi
+
+Request: "array reduce in js include comments"
+Snippet:// reduce takes an array and reduces it to a single value
+const array = [1, 2, 3, 4];
+const reducedValue = array.reduce((accumulator, currentValue) => {
+  // accumulator is the running total of the reduction
+  // currentValue is the current element being processed in the array
+  return accumulator + currentValue;
+}, 0); // 0 is the initial value of the accumulator
+
+console.log(reducedValue); // 10
+
+Request: "Print all variables of a list in pseudo-code"
+Snippet:for each variable in list:
+    print variable
+end
+---
+Request: "{request}"
+Snippet:`;
+
+export default async (request, openai) => {
+  if (!request || request.length < 3) {
+    return "";
+  }
+
+  const { data: response } = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: PROMPT_TEMPLATE.replace("{request}", request),
+    temperature: 0,
+    max_tokens: 1600,
+    top_p: 1,
+    frequency_penalty: 0.2,
+    presence_penalty: 0,
+    stop: ["---"],
+  });
+
+  if (!response.choices[0]) {
+    return "";
+  }
+
+  return response.choices[0].text;
+};
