00e89d034d5a99ecf6d0c98ba658e875663f3437
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-04-09 22:45:12 +0200

    feat: enable usage with cli arguments

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 #! /usr/bin/env node
 
+import { program } from "commander";
 import { config } from "dotenv";
 import { Configuration, OpenAIApi } from "openai";
+
+import packageJson from "./package.cjs";
 
 import requestSnippet from "./snippet-requester.js";
 
@@ -18,12 +21,25 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-process.stdin.on("data", async (data) => {
-  const snippet = await requestSnippet(data?.toString(), openai);
+program
+  .name("snipgpt")
+  .version(packageJson.version, "-v, --version")
+  .description(packageJson.description)
+  .argument("[request]", "Use arguments as string to request snippets")
+  .parse();
 
-  if (!snippet) {
-    return;
+if (program.args.length) {
+  const snippet = await requestSnippet(program.args.join(" "), openai);
+
+  if (snippet) {
+    console.log(snippet);
   }
+} else {
+  process.stdin.on("data", async (data) => {
+    const snippet = await requestSnippet(data?.toString(), openai);
 
-  console.log(snippet);
-});
+    if (snippet) {
+      console.log(snippet);
+    }
+  });
+}
