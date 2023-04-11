001d55cdcd1bf449032b8c777f5f8bc7bcda31fb
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-04-11 23:09:01 +0200

    tests: add jest config and basic tests

---
+++ src/snippet-requester.spec.js
@@ -0,0 +1,45 @@
+import snippetRequest from "./snippet-requester";
+
+describe("snippetRequest", () => {
+  it("returns a code snippet for a given request", async () => {
+    const request = "bash: Check if a variable is not empty";
+    const expectedSnippet =
+      'if [ -n "$my_variable" ]; then\n    echo "my_variable is not empty"\nelse\n    echo "my_variable is empty"\nfi';
+
+    const openai = {
+      createCompletion: jest.fn().mockResolvedValue({
+        data: {
+          choices: [{ text: expectedSnippet }],
+        },
+      }),
+    };
+
+    expect(await snippetRequest(request, openai)).toEqual(expectedSnippet);
+    expect(openai.createCompletion).toHaveBeenCalledWith(
+      expect.objectContaining({
+        prompt: expect.stringContaining(request),
+      })
+    );
+  });
+
+  it("returns empty response for a request that is smaller than a word", async () => {
+    const request = "fo";
+    const expectedSnippet = "";
+
+    const openai = {
+      createCompletion: jest.fn().mockResolvedValue({
+        data: {
+          choices: [{ text: expectedSnippet }],
+        },
+      }),
+    };
+
+    expect(await snippetRequest(request, openai)).toEqual(expectedSnippet);
+
+    expect(openai.createCompletion).not.toHaveBeenCalledWith(
+      expect.objectContaining({
+        prompt: expect.stringContaining(request),
+      })
+    );
+  });
+});
